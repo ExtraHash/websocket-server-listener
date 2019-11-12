d804e2315c7d764fbfd0f1bf4e8b07dc7a6d5855
ender <extrahash@protonmail.com>
2019-11-11 17:01:29 -0700

    add a listening message

--- index.ts
+++ index.ts
@@ -2,6 +2,10 @@ import WebSocket from 'ws';
 
 const wss = new WebSocket.Server({ port: 41920 });
 
+wss.on('listening', function listening() {
+  console.log('Server listening on port 41920')
+})
+
 wss.on('connection', function connection(ws) {
 
 
